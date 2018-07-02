0347875814e373fb13c5a225e28a8a7faa9dda5c
NS-159 <NS-159@NS-159-HP>
2018-07-01 21:46:35 -0500

    Adding Hibernate capabilities

--- gefa/src/main/java/com/eglobal/gefa/dto/RoleDTO.java
+++ gefa/src/main/java/com/eglobal/gefa/dto/RoleDTO.java
@@ -7,6 +7,36 @@ public class RoleDTO  implements Serializable {
 	 * UID for serializable classes.
 	 */
 	private static final long serialVersionUID = 3984778276949983950L;
-	String id;
-	String roleName;
+	/**
+	 * Role Id
+	 */
+	private String roleId;
+	/**
+	 * Role name
+	 */
+	private String roleName;
+	/**
+	 * @return the roleId
+	 */
+	public String getRoleId() {
+		return roleId;
+	}
+	/**
+	 * @param roleId the roleId to set
+	 */
+	public void setRoleId(final String roleId) {
+		this.roleId = roleId;
+	}
+	/**
+	 * @return the roleName
+	 */
+	public String getRoleName() {
+		return roleName;
+	}
+	/**
+	 * @param roleName the roleName to set
+	 */
+	public void setRoleName(final String roleName) {
+		this.roleName = roleName;
+	}
 }
--- gefa/src/main/java/com/eglobal/gefa/view/UserCatalog.java
+++ gefa/src/main/java/com/eglobal/gefa/view/UserCatalog.java
@@ -1,5 +1,6 @@
 package com.eglobal.gefa.view;
 
 public class UserCatalog {
+	
 
 }
